a83e7fecf499c4460260aba4bba10b41af26e420
Miguel <migue2801224@gmail.com>
2024-10-30 19:53:00 -0500

    Eliminar propiedad

--- objetos.js
+++ objetos.js
@@ -8,13 +8,17 @@ const usuario={
     }
 }
 //Imprimir a las propiedades de un objeto
-//console.log(usuario)
+console.log(usuario)
 
 //Imprimir propiedades por separado
-//console.log(usuario.nombre)
-//console.log(usuario.edad)
-//console.log(usuario.trabaja)
+console.log(usuario.nombre)
+console.log(usuario.edad)
+console.log(usuario.trabaja)
 
 //agregar propidades a un objeto
 usuario.carrera="ing de sistemas"
+console.log(usuario)
+
+//Eliminar una propiedad
+delete usuario.trabaja
 console.log(usuario)
