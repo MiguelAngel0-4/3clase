b843162b4949c4d7601bbd60884560132ab1f960
Miguel <migue2801224@gmail.com>
2024-10-30 19:25:56 -0500

    se creo objeto

--- objetos.js
+++ objetos.js
@@ -4,9 +4,13 @@ const usuario={
     trabaja: true,
 }
 //Imprimir a las propiedades de un objeto
-console.log(usuario)
+//console.log(usuario)
 
 //Imprimir propiedades por separado
-console.log(usuario.nombre)
-console.log(usuario.edad)
-console.log(usuario.trabaja)
+//console.log(usuario.nombre)
+//console.log(usuario.edad)
+//console.log(usuario.trabaja)
+
+//agregar propidades a un objeto
+usuario.carrera="ing de sistemas"
+console.log(usuario)
